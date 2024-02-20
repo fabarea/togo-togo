85e29df9aed7ea2a99b305a2a80af5f0dd2c2dd6
uid210565 <uid210565@h2web328.infomaniak.ch>
2024-02-20 21:49:56 +0100

    feat: add french pack

--- config/system/settings.php
+++ config/system/settings.php
@@ -13,10 +13,18 @@ return [
         'Connections' => [
             'Default' => [
                 'charset' => 'utf8mb4',
+                'driver' => 'mysqli',
                 'tableoptions' => [
                     'charset' => 'utf8mb4',
                     'collate' => 'utf8mb4_unicode_ci',
                 ],
+            ],
+        ],
+    ],
+    'EXTCONF' => [
+        'lang' => [
+            'availableLanguages' => [
+                'fr',
             ],
         ],
     ],
